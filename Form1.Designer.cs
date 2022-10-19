b255167ad30da221934683698b93f0eff5c904bf
guillermoarre <guillermo.arredondo247@gmail.com>
2022-10-19 11:01:49 -0500

    Se agrega el proyecto

---
+++ Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Practica3_Agentes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbx1 = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.tbx2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menúToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGrid1 = new System.Windows.Forms.DataGridView();
+            this.dataGrid2 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(53, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "No. de Entradas";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tbx1
+            // 
+            this.tbx1.Location = new System.Drawing.Point(183, 49);
+            this.tbx1.Name = "tbx1";
+            this.tbx1.Size = new System.Drawing.Size(100, 20);
+            this.tbx1.TabIndex = 1;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(111, 92);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(97, 50);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = "Generar \r\nTablas";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // tbx2
+            // 
+            this.tbx2.Location = new System.Drawing.Point(182, 174);
+            this.tbx2.Name = "tbx2";
+            this.tbx2.Size = new System.Drawing.Size(100, 20);
+            this.tbx2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(107, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Umbral";
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(111, 200);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(97, 44);
+            this.btn2.TabIndex = 5;
+            this.btn2.Text = "Neurona";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(600, 213);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(79, 31);
+            this.btn3.TabIndex = 6;
+            this.btn3.Text = "Limpiar";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menúToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(691, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menúToolStripMenuItem
+            // 
+            this.menúToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salirToolStripMenuItem});
+            this.menúToolStripMenuItem.Name = "menúToolStripMenuItem";
+            this.menúToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menúToolStripMenuItem.Text = "Menú";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(96, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // dataGrid1
+            // 
+            this.dataGrid1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGrid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid1.Location = new System.Drawing.Point(366, 49);
+            this.dataGrid1.Name = "dataGrid1";
+            this.dataGrid1.Size = new System.Drawing.Size(212, 195);
+            this.dataGrid1.TabIndex = 9;
+            // 
+            // dataGrid2
+            // 
+            this.dataGrid2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGrid2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid2.Location = new System.Drawing.Point(27, 292);
+            this.dataGrid2.Name = "dataGrid2";
+            this.dataGrid2.Size = new System.Drawing.Size(652, 219);
+            this.dataGrid2.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(362, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Pesos";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(23, 257);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Tabla de verdad";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(691, 561);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGrid2);
+            this.Controls.Add(this.dataGrid1);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbx2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.tbx1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Practica 3, Red Neuronal";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbx1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.TextBox tbx2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menúToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGrid1;
+        private System.Windows.Forms.DataGridView dataGrid2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica3_Agentes
+{
+    public partial class Form1 : Form
+    {
+        int m1, n1;
+        int m2, n2;
+        int entradas;
+        int[] Mat;
+        double Umbral;
+        double[] Acumulador;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            dataGrid1.Columns.Clear();
+            dataGrid1.Rows.Clear();
+            dataGrid2.Columns.Clear();
+            dataGrid2.Rows.Clear();
+
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            int i, j;
+            Mat = new int[m1];
+            Acumulador = new double[m2];
+            //Recolecta valores
+            try
+            {
+                Umbral = double.Parse(tbx2.Text);
+                for (i = 0; i < m1; i++)
+                {
+                    Mat[i] = int.Parse(dataGrid1.Rows[i].Cells[0].Value.ToString());
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ingresa Umbral!!!");
+
+            }
+            //Comienza a resolver
+            //Paso 1, multiplica los valores por el valor de la tabla
+            for (i = 0; i < m2; i++)   
+            {
+                for (j = 0; j < n2-1; j++)
+                {
+                    Acumulador[i] += double.Parse(dataGrid2.Rows[i].Cells[j].Value.ToString()) * Convert.ToDouble(Mat[j]);
+                }
+
+            }
+
+            //Paso 2, compara los valores con el umbral y colocalos en Y
+            for (i = 0; i < m2; i++)
+            {
+                if (Acumulador[i] > Umbral)
+                {
+                    dataGrid2.Rows[i].Cells[n2-1].Value = 1;
+                }
+                else 
+                {
+                    dataGrid2.Rows[i].Cells[n2-1].Value = 0;
+                }
+
+            }
+            MessageBox.Show("Red Neuronal Resuelta!!");
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            //Borrar datos en la memoria
+            dataGrid1.Columns.Clear();
+            dataGrid1.Rows.Clear();
+            dataGrid2.Columns.Clear();
+            dataGrid2.Rows.Clear();
+
+
+            try
+            {
+
+                //Graficar tabla de valores
+                entradas = int.Parse(tbx1.Text);
+                m1 = entradas;
+                n1 = 1;
+                dataGrid1.RowCount = m1;
+                dataGrid1.ColumnCount = n1;
+                dataGrid1.Columns[0].HeaderText = "Pesos, Wn";
+                //Graficar tabla de verdad
+                double aux;
+                aux = Math.Pow(2.0, Convert.ToDouble(entradas));
+                m2 = Convert.ToInt32(aux);
+                n2 = entradas + 1;
+                //Columnas
+                for (int j = 0; j < n2 + 1; j++)
+                {
+                    dataGrid2.ColumnCount = j;
+
+                }
+                for (int j = 0; j < n2 - 1; j++)
+                {
+                    dataGrid2.Columns[j].HeaderText = "x" + (j + 1).ToString();
+                }
+                dataGrid2.Columns[n2 - 1].HeaderText = "Y";
+
+
+                //Filas
+                for (int i = 1; i < m2 + 1; i++)
+                {
+                    dataGrid2.RowCount = i;
+
+                }
+                //dataGrid2.Rows[0].Cells[0].Value = 0;
+                llenarTablaDeVerdad(m2, n2 - 1);
+            }
+            catch
+            {
+                MessageBox.Show("Ingresa No. de entradas!!!");
+            }
+            
+
+
+
+
+        }
+
+        public void llenarTablaDeVerdad(int m, int n)
+        {
+            MessageBox.Show("Tablas Generadas \n" +
+                "m1 =" + m1.ToString() + " n1 =" + n1.ToString() + "\n" +
+                "m2 =" + m2.ToString() + " n2 =" + (n2).ToString());
+            //MessageBox.Show("Filas = " + m.ToString() + " Col =" + n.ToString());
+            int i, j, c = 0, x2 = 0;
+            int x1 = m / 2;
+
+            for (j = 0; j < n; j++)
+            {
+                for (i = 0; i < m; i++)
+                {
+                    if (x2 == 0)
+                    {
+                        dataGrid2.Rows[i].Cells[j].Value = 0;
+                        c++;
+                    }
+                    else if (x2 == 1)
+                    {
+                        dataGrid2.Rows[i].Cells[j].Value = 1;
+                        c--;
+                    }
+                    if (c == x1)
+                    {
+                        //llegaste a la mitad de la tabla
+                        x2 = 1;
+                    }
+                    if (c == 0)
+                    {
+                        //terminaste con la columa Xn
+                        x2 = 0;
+                    }
+                }
+                x1 = x1 / 2;
+            
+            }
+
+        }
+    }
+}
